f3df1e39f79020dea9d4627bff5f5a8a13af57e8
David Moina <davemolina777@gmail.com>
2023-07-12 17:24:28 -0500

    Import pages components and Canvas component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-function App() {
-  const [count, setCount] = useState(0)
+import Canvas from './canvas'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
 
+function App() {
   return (
     <div>
       <h1 className="head-text">
