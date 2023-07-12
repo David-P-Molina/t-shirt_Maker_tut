62c5e8dec3662811a4c516cc26944c15299508c3
David Moina <davemolina777@gmail.com>
2023-07-12 17:26:16 -0500

    Add component elements to web page

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      <h1 className="head-text">
-        React App
-      </h1>
-    </div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
         
   )
 }
