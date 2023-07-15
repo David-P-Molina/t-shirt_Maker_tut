af240f4f49e91e4f6f8839794a280c6ab169c9c6
David Moina <davemolina777@gmail.com>
2023-07-14 20:24:41 -0500

    Add logo as header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,10 @@ const Home = () => {
         {snap.intro && (
             <motion.section className="home" {...slideAnimation('left')}>
                 <motion.header>
-                    
+                    <img 
+                        src='./threejs.png' 
+                        alt='logo' 
+                        className="w-8 h-8 object-contain"/>
                 </motion.header>
             </motion.section>
         )}
